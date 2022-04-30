c73956868c35de20087c998f1b622d0f2ba4e287
Claudia-Oliveira <claudia.oliveira@mediamonks.com>
2022-04-30 16:45:34 +0200

    created the SectionCases component

---
+++ src/components/SectionCases.js
@@ -0,0 +1,47 @@
+import styles from './SectionCases.module.scss';
+
+import React, { Component } from 'react';
+import Button from './Button';
+import classNames from 'classnames';
+
+export default class SectionCases extends Component {
+    render() {
+        const { t } = this.props;
+
+        return (
+            <section className={classNames(styles.sectionCases__container, classNames)}>
+                <h2 className={styles.sectionCases__title}>{t('home:cases__title')}</h2>
+
+                <ul className={`${styles.list} ${styles.sectionCases__links} `}>
+                    {t('home:navigation', { returnObjects: true }).map((item) => {
+                        return (
+                            <li key={item.button.copy} className={`${styles.item} ${styles.sectionCases__button_li}`}>
+                                <Button href={item.button.href} className={styles.sectionCases__button_cases}>
+                                    <span className={styles.sectionCases__button_order}>{item.button.number}</span>
+                                    <span className={styles.sectionCases__button_label}>{item.button.copy}</span>
+                                    <svg className={styles.sectionCases__button_icon} viewBox="0 0 16 5">
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M0.391,2.35 L10.945,1.976 C10.945,1.196 10.942,0.747 10.914,0.53 C12.586,0.936 14.47,1.696 15.739,2.456 C14.83,3.270 12.634,4.12 10.914,4.859 C10.998,4.105 10.945,3.765 10.945,2.936 L0.391,2.821 L0.391,2.35 Z"
+                                        />
+                                    </svg>
+                                </Button>
+                            </li>
+                        );
+                    })}
+                    <li className={`${styles.item} ${styles.sectionCases__button_li}`}>
+                        <Button href="/the-work" className={styles.sectionCases__button_cases_work}>
+                            <span className={styles.sectionCases__button_label}>{t('home:button__more_work')}</span>
+                            <svg className={styles.sectionCases__button_icon_work} viewBox="0 0 16 5">
+                                <path
+                                    fillRule="evenodd"
+                                    d="M0.391,2.35 L10.945,1.976 C10.945,1.196 10.942,0.747 10.914,0.53 C12.586,0.936 14.47,1.696 15.739,2.456 C14.83,3.270 12.634,4.12 10.914,4.859 C10.998,4.105 10.945,3.765 10.945,2.936 L0.391,2.821 L0.391,2.35 Z"
+                                />
+                            </svg>
+                        </Button>
+                    </li>
+                </ul>
+            </section>
+        );
+    }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head';
 import React, { Component } from 'react';
 import styles from './index.module.scss';
-import Button from 'components/Button';
 import HeaderLinks from 'components/HeaderLinks';
 import { withTranslation, getTranslation } from 'utils/translations/i18n';
 import SectionStudio from 'components/SectionStudio';
+import SectionCases from 'components/SectionCases';
 
 class Home extends Component {
     render() {
@@ -37,41 +37,7 @@ class Home extends Component {
                     <div className={styles.actualVideo}></div>
                 </div> */}
 
-                <div className={styles.home__cases}>
-                    <h2 className={styles.home__pages_title}>{t('home:cases__title')}</h2>
-
-                    <ul className={`${styles.list} ${styles.home__links} `}>
-                        {t('home:navigation', { returnObjects: true }).map((item) => {
-                            return (
-                                <li key={item.button.copy} className={`${styles.item} ${styles.home__button_li}`}>
-                                    <Button href={item.button.href} className={styles.home__button_cases}>
-                                        <span className={styles.home__button_order}>{item.button.number}</span>
-                                        <span className={styles.home__button_label}>{item.button.copy}</span>
-                                        <svg className={styles.home__button_icon} viewBox="0 0 16 5">
-                                            <path
-                                                fillRule="evenodd"
-                                                d="M0.391,2.35 L10.945,1.976 C10.945,1.196 10.942,0.747 10.914,0.53 C12.586,0.936 14.47,1.696 15.739,2.456 C14.83,3.270 12.634,4.12 10.914,4.859 C10.998,4.105 10.945,3.765 10.945,2.936 L0.391,2.821 L0.391,2.35 Z"
-                                            />
-                                        </svg>
-                                    </Button>
-                                </li>
-                            );
-                        })}
-                        {/* <li className={`${styles.item} ${styles.home__button_li}`}>
-                            <Button href="/the-work" className={styles.home__button_cases_work}>
-                                <span className={styles.home__button_order}>{t('home:button__more_work_number')}</span>
-                                <span className={styles.home__button_label}>{t('home:button__more_work')}</span>
-                                <svg className={styles.home__button_icon_work} viewBox="0 0 16 5">
-                                    <path
-                                        fillRule="evenodd"
-                                        fill="rgb(48, 48, 48)"
-                                        d="M0.391,2.35 L10.945,1.976 C10.945,1.196 10.942,0.747 10.914,0.53 C12.586,0.936 14.47,1.696 15.739,2.456 C14.83,3.270 12.634,4.12 10.914,4.859 C10.998,4.105 10.945,3.765 10.945,2.936 L0.391,2.821 L0.391,2.35 Z"
-                                    />
-                                </svg>
-                            </Button>
-                        </li> */}
-                    </ul>
-                </div>
+                <SectionCases t={t} />
             </div>
         );
     }
